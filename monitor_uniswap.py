85e3a893200b142046047746b9fb4f06db50b42c
tecnolom <tecnolom@gmail.com>
2025-09-05 20:11:34 +0100

    Update monitor_uniswap.py

--- monitor_uniswap.py
+++ monitor_uniswap.py
@@ -130,8 +130,8 @@ if driver:
     fees_value, range_status = get_fees_and_range_status(driver)
     
     if fees_value:
-        message = f"🦄 <b>Ganhos Uniswap...</b>\n\n"
-        message += f"💵 Total disponível para soleta: <b>${fees_value:.2f}</b>\n"
+        message = f"🦄 <b>Slerta de Ganhos...</b>\n\n"
+        message += f"💵 Total disponível para coleta: <b>${fees_value:.2f}</b>\n"
         
         if "🟢" in range_status:
             message += f"🟢 Pool Status: Dentro do Range"
